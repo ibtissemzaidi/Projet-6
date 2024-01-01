2a00456d0f3d3826c3027850fa2264d7d56354c3
zaidi ibtissem <awessale@gmail.com>
2024-01-01 08:14:19 +0100

    modifier fichier login

---
+++ FrontEnd/login.js
@@ -0,0 +1,37 @@
+const submit = document.getElementById("submit");
+const form = document.querySelector(".login__form");
+let users = localStorage.getItem("token");
+form.addEventListener("submit", function () {
+  const email = document.getElementById("email").value;
+  const motDePasse = document.getElementById("password").value;
+  // Données à envoyer dans la requête POST
+  const donnees = {
+    email: email,
+    password: motDePasse,
+  };
+
+  // Effectuer la requête POST à l'aide de fetch
+  fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(donnees),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        alert("Mot de passe ou nom d'utlisateur est incorret");
+      }
+    })
+    .then((userInfo) => {
+      const token = userInfo.token;
+      console.log("token");
+      sessionStorage.setItem("token", token);
+      window.location.href = "index.html";
+    })
+    .catch((erreur) => {
+      console.error("Erreur :", erreur);
+    });
+});
---
+++ FrontEnd/script.js
@@ -0,0 +1,61 @@
+let data = [];
+async function getData() {
+  const api = "http://localhost:5678/api/works";
+  try {
+    const response = await fetch(api);
+    data = await response.json();
+    console.log(data);
+
+    generProjets(data);
+  } catch (error) {
+    console.error(
+      "Une erreur s'est produite lors de la récupération des données :",
+      error
+    );
+  }
+}
+getData();
+function generProjets(data) {
+  //effacer le contenue de la balise body
+  document.querySelector(".gallery").innerHTML = "";
+
+  // Génère les projets
+
+  for (let i = 0; i < data.length; i++) {
+    const figure = document.createElement("figure");
+    const sectionProjets = document.querySelector(".gallery");
+    // figure.classList.add(`js-projet-${data[i].id}`); // Ajoute l'id du projet pour le lien vers la modale lors de la supression
+    const img = document.createElement("img");
+    img.src = data[i].imageUrl;
+    img.alt = data[i].title;
+    figure.appendChild(img);
+
+    const figcaption = document.createElement("figcaption");
+    figcaption.innerHTML = data[i].title;
+    figure.appendChild(figcaption);
+    sectionProjets.appendChild(figure);
+  }
+}
+// Affichage des filtres
+async function getcategories() {
+  const response = await fetch("http://localhost:5678/api/categories");
+  const categories = await response.json();
+  console.log(categories);
+  const btnTous = document.querySelector(".filters button");
+  btnTous.addEventListener("click", (e) => {
+    generProjets(data);
+  });
+  categories.forEach((category) => {
+    const btn = document.createElement("button");
+    btn.textContent = category.name;
+    btn.id = category.id;
+    const filters = document.querySelector(".filters");
+    filters.appendChild(btn);
+    //filtrage au click sur le bouton par categories
+    btn.addEventListener("click", (e) => {
+      const filterdata = data.filter((d) => d.category.id === category.id);
+      generProjets(filterdata);
+    });
+  });
+}
+getcategories();
